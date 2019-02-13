6af9329e706a4fa53265de494ecec8633f4ae5e7
Administrator <Administrator@USER-PC>
2019-02-13 11:58:31 +0900

    StringType 파일수정

--- src/data/StringType.java
+++ src/data/StringType.java
@@ -5,6 +5,10 @@ public class StringType {
 	public static void main(String[] args) {
 		String str = "123";
 		System.out.println(str+123);
+		String str2 = new String("123");
+				// == 바라보고 있는 주소? 가 같은지 .
+				System.out.println(str.contentEquals(str2));
+				System.out.println(str==str2);
 
 		
 	}
