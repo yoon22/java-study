de10f10d3717270aba756b6289d1bd9c77d44615
Administrator <Administrator@USER-PC>
2019-02-13 11:37:29 +0900

    첫 자바프로젝트 깃 연동

---
+++ src/data/StringType.java
@@ -0,0 +1,11 @@
+package data;
+
+public class StringType {
+	
+	public static void main(String[] args) {
+		String str = "123";
+		System.out.println(str+123);
+
+		
+	}
+}
---
+++ src/data/Type01.java
@@ -0,0 +1,29 @@
+package data;
+
+public class Type01 {
+	
+	public static void main(String[] args) {
+		//문자
+		char c = 'a';
+		
+		//숫자 - 정수
+		byte b = 127;
+		b = -128;
+		
+		short sh = 123;
+		
+		int i = 12123123;
+		
+		long l = 1000000000000000l;
+		
+		//숫자 - 실수
+		float f =1.2f;
+		double db = 1.2;
+		
+		//논리형
+		boolean bl = true;
+		
+		
+	}
+
+}
